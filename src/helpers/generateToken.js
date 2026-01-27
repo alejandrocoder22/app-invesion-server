805138f16f0ee93712cdd92c3890e8b3b6257a88
alejandrocoder22 <alejandro2210lp@gmail.com>
2026-01-27 22:26:42 +0100

    Fixed

--- src/helpers/generateToken.js
+++ src/helpers/generateToken.js
@@ -10,9 +10,8 @@ export const generateToken = (res, username, isAdmin, userId) => {
 
   res.cookie('x-token', token, {
     httpOnly: true,
-    // secure: process.env.NODE_ENV === 'production',
-    secure: true,
-    sameSite: 'None',
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'Lax',
     maxAge: 24 * 60 * 60 * 1000 // 1 día
   })
 }
